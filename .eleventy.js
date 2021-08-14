0655034eaa34cdea2aeab9b9fafb3c1fd8995edf
Deborah Barnard <deborah@starfallprojects.co.uk>
2021-08-14 17:56:13 +0100

    commit the starter

---
+++ .eleventy.js
@@ -0,0 +1,41 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('src/css');
+    eleventyConfig.addPassthroughCopy('src/js');
+    eleventyConfig.addPassthroughCopy('src/images');
+    eleventyConfig.addPassthroughCopy('src/documents');
+    eleventyConfig.setDataDeepMerge(true);
+
+    // Date formats
+    eleventyConfig.addFilter("readableDate", dateObj => {
+       return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
+    });
+    eleventyConfig.addFilter("justTheYear", dateObj => {
+        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy');
+    });
+    
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if( n < 0 ) {
+            return array.slice(n);
+        }
+        return array.slice(0, n);
+    });
+
+    // Return the smallest number argument
+    eleventyConfig.addFilter("min", (...numbers) => {
+        return Math.min.apply(null, numbers);
+    });
+
+    return {
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+        passthroughFileCopy: true,
+        dir: {
+            input: "src",
+            output: "dist"
+        }
+    }
+}
