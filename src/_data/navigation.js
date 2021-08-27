93220834fac9285f5b3e1949d56f8977cf7d96c0
Deborah Barnard <deborah@starfallprojects.co.uk>
2021-08-27 18:56:53 +0100

    navigation

---
+++ src/_data/navigation.js
@@ -0,0 +1,18 @@
+// DOCS:
+module.exports = [
+    {
+        title: "Home",
+        slug: ""
+    },
+    {
+        slug: "quickstart"
+    },
+    {
+        title: "Docs",
+        slug: "docs",
+        children: [
+
+        ]
+    }
+
+]
